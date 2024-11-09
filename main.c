0183e7d632ad7143d8bf988ccff15e6034e5cc8d
omisso <andresafira2004@gmail.com>
2024-11-09 15:26:07 -0300

    feat: add singleplayer mode

--- main.c
+++ main.c
@@ -27,14 +27,14 @@ int ship_is_valid(int* board, int i, int j, int size, int hor){
   int b = j;
   for (int d = 0; d < size; ++d){
     if (hor) {
-      b += d;
+      b += 1;
     } else {
-      a += d;
+      a += 1;
     }
 
-    valid = valid & is_valid(board, a, b);
+    valid = valid && is_valid(board, a, b);
     if (valid) {
-      valid = valid & (get_pos(board, a, b) == 0);
+      valid = valid && (get_pos(board, a, b) == 0);
     } else {
       return 0;
     }
@@ -48,7 +48,7 @@ void init_board(int* board){
   }
 }
 
-void print_board(int* board){
+void print_board(int* board, int shipping){
   int value = 0;
   for (int i = 0; i < 45; ++i){
     printf("=");
@@ -58,11 +58,11 @@ void print_board(int* board){
     printf("|| ");
     for (int j = 0; j < 20; ++j){
       value = get_pos(board, i, j);
-      if (value == 0){
+      if (value == 0 || value == 1 && !shipping){
         printf("  ");
-      } else if (value == 1) {
+      } else if (value == 3 || value == 1 && shipping) {
         printf("O ");
-      } else {
+      } else if (value == 2) {
         printf("X ");
       }
     }
@@ -88,6 +88,8 @@ void shipping(int* board, int* ships_size){
   int ships_quant[6];
 
   init_ships_quant(ships_quant);
+  
+  print_board(board, 1);
 
   while (ships_quant[5] > 0){
     do {
@@ -115,30 +117,65 @@ void shipping(int* board, int* ships_size){
 
     input_ship(board, i, j, ships_size[ship], direction_horizontal);
 
-    print_board(board);
+    print_board(board, 1);
   }
+}
+
+void AI_shipping(int* board, int* ships_size) {
+  
+
+};
+
+void test_helper(int* board1, int* board2) {
+  input_ship(board1, 0, 0, 5, 0);
+  input_ship(board1, 2, 2, 5, 0);
+  input_ship(board1, 4, 4, 3, 0);
+  input_ship(board1, 6, 6, 3, 0);
+  input_ship(board1, 8, 8, 3, 0);
+  input_ship(board1, 10, 10, 3, 0);
+  input_ship(board1, 12, 12, 2, 0);
+  input_ship(board1, 14, 14, 2, 0);
+  
+  input_ship(board2, 0, 0, 5, 1);
+  input_ship(board2, 2, 2, 5, 1);
+  input_ship(board2, 4, 4, 3, 1);
+  input_ship(board2, 6, 6, 3, 1);
+  input_ship(board2, 8, 8, 3, 1);
+  input_ship(board2, 10, 10, 3, 1);
+  input_ship(board2, 12, 12, 2, 1);
+  input_ship(board2, 14, 14, 2, 1);
 }
 
 int main(){
   int board1[400];
   int board2[400];
-  int ships_quant[6];
   int ships_size[5] = {5, 4, 3, 3, 2};
 
-  init_ships_quant(ships_quant);
+  int test_shoot = 1;
+
   init_board(board1);
   init_board(board2);
+
+  int singleplayer;
+
+  printf("Choose the Gamemode: (0: multiplayer, 1: singleplayer)");
+  scanf("%d", &singleplayer);
+
+  if (test_shoot) {
+    test_helper(board1, board2);
+  } else {
+    shipping(board1, ships_size);
+    printf("\nSwitch players now (use DC)\n");
+    shipping(board2, ships_size);
+  }
   
-  shipping(board1, ships_size);
-  printf("\nSwitch players now (use DC)\n");
-  shipping(board2, ships_size);
-  
-  int turn = 0, win = 0, i, j, valid;
-  int** current_board;
+  int turn = 0, i, j, valid, cur_pos;
+  int n_pos[2] = {25, 25};
+  int* current_board[2];
   current_board[0] = board2;
   current_board[1] = board1;
 
-  while (!win) {
+  while (n_pos[0] > 0 && n_pos[1] > 0) {
     do {
       printf("\nRow to shoot: ");
       scanf("%d", &i);
@@ -147,7 +184,8 @@ int main(){
 
       valid = is_valid(current_board[turn], i, j);
       if (valid){
-        valid = (get_pos(current_board[turn], i, j) != 2);
+        cur_pos = get_pos(current_board[turn], i, j);
+        valid = !(cur_pos == 2 || cur_pos == 3);
       }
 
       if (!valid) {
@@ -155,10 +193,22 @@ int main(){
       }
     } while (!valid);
     
-    // Make logic for shooting the ships
+    if (cur_pos == 0) {
+      put_pos(current_board[turn], i, j, 2);
+    } else {
+      put_pos(current_board[turn], i, j, 3);
+      n_pos[turn] --;
+    }
 
+    print_board(current_board[turn], 0);
 
     turn = !turn;
+  }
+
+  if (n_pos[0] == 0) {
+    printf("\n\n The Winner is Player 1");
+  } else {
+    printf("\n\n The Winner is Player 2");
   }
 
   return 0;
